3e48a859e099f87e424a8b2efbbab4a6ab803bdd
Peter Taoussanis <ptaoussanis@gmail.com>
2016-07-24 14:59:45 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [com.taoensso/encore "2.64.1"]]
+   [com.taoensso/encore "2.67.1"]]
 
   :plugins
   [[lein-pprint  "1.1.2"]
@@ -28,13 +28,13 @@
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha9"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
    :test {:dependencies [[org.clojure/test.check "0.9.0"]]}
    :dev
    [:1.9 :test :server-jvm
     {:dependencies
      [[org.clojure/clojurescript "1.9.93"]
-      [com.taoensso/timbre       "4.5.1"]]
+      [com.taoensso/timbre       "4.7.2"]]
 
      :plugins
      [;; These must be in :dev, Ref. https://github.com/lynaghk/cljx/issues/47:
