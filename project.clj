b2c42186752ddbd2945f7090f097064a2cbe1da8
Peter Taoussanis <peter@taoensso.com>
2022-10-26 10:27:33 +0200

    [nop] Update project.clj, build.yml

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
             :distribution :repo
             :comments "Same as Clojure"}
   :min-lein-version "2.3.3"
-  :global-vars {*warn-on-reflection* true
-                *assert*             true}
+  :global-vars
+  {*warn-on-reflection* true
+   *assert*             true
+   *unchecked-math*     false #_:warn-on-boxed}
 
   :dependencies
-  [[com.taoensso/encore "3.26.0"]]
+  [[com.taoensso/encore "3.31.0"]]
 
   :plugins
   [[lein-pprint    "1.3.2"]
@@ -22,39 +24,45 @@
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :provided {:dependencies [[org.clojure/clojure       "1.7.0"]
-                             [org.clojure/clojurescript "1.11.60"]]}
-   :1.7      {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8      {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9      {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10     {:dependencies [[org.clojure/clojure "1.10.1"]]}
-   :1.11     {:dependencies [[org.clojure/clojure "1.11.1"]]}
+   :provided {:dependencies [[org.clojure/clojurescript "1.11.60"]
+                             [org.clojure/clojure       "1.11.1"]]}
+   :c1.11    {:dependencies [[org.clojure/clojure       "1.11.1"]]}
+   :c1.10    {:dependencies [[org.clojure/clojure       "1.10.1"]]}
+   :c1.9     {:dependencies [[org.clojure/clojure       "1.9.0"]]}
+
    :depr     {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
-   :dev      [:1.11 :test :server-jvm :depr]
-   :test     {:dependencies
-              [[org.clojure/test.check "1.1.1"]
-               [com.taoensso/timbre "5.2.1"]]}}
+   :dev      [:c1.11 :test :server-jvm :depr]
+   :test     {:dependencies [[org.clojure/test.check "1.1.1"]
+                             [com.taoensso/timbre    "5.2.1"]]}}
+
+  :test-paths ["src" "test"]
 
   :cljsbuild
-  {:test-commands
-   {"node"    ["node" :node-runner "target/main.js"]
-    "phantom" ["phantomjs" :runner "target/main.js"]}
-
+  {:test-commands {"node" ["node" "target/test.js"]}
    :builds
    [{:id :main
-     :source-paths ["src" "test"]
+     :source-paths ["src"]
      :compiler
      {:output-to "target/main.js"
-      :optimizations :advanced
-      :pretty-print false}}]}
+      :optimizations :advanced}}
+
+    {:id :test
+     :source-paths ["src" "test"]
+     :compiler
+     {:output-to "target/test.js"
+      :target :nodejs
+      :optimizations :simple}}]}
 
   :aliases
   {"start-dev"  ["with-profile" "+dev" "repl" ":headless"]
-   "build-once" ["cljsbuild" "once"]
-   "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]
-   "test-all"   ["do" "clean,"
-                 "with-profile" "+1.10:+1.9:+1.8:+1.7" "test,"
-                 "with-profile" "+test" "cljsbuild"    "test"]}
+   "deploy-lib" ["do" ["build-once"] ["deploy" "clojars"] ["install"]]
+   "build-once" ["do" ["clean"] "cljsbuild" "once"]
+
+   "test-cljs"  ["with-profile" "+test" "cljsbuild" "test"]
+   "test-all"
+   ["do" ["clean"]
+    "with-profile" "+c1.11:+c1.10:+c1.9" "test,"
+    "test-cljs"]}
 
   :repositories
   {"sonatype-oss-public"
