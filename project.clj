872747ec838b075bf9fbdd549ab58bcdbf636a49
Peter Taoussanis <peter@taoensso.com>
2023-11-03 12:09:05 +0100

    [nop] Update project template

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject com.taoensso/tufte "2.6.3"
   :author "Peter Taoussanis <https://www.taoensso.com>"
-  :description "Simple performance monitoring for Clojure/Script applications"
+  :description "Simple performance monitoring library for Clojure/Script"
   :url "https://github.com/taoensso/tufte"
 
   :license
   {:name "Eclipse Public License - v 1.0"
    :url  "https://www.eclipse.org/legal/epl-v10.html"}
+
+  :test-paths ["test" #_"src"]
 
   :dependencies
   [[com.taoensso/encore "3.68.0"]]
@@ -14,11 +16,22 @@
   {;; :default [:base :system :user :provided :dev]
    :provided {:dependencies [[org.clojure/clojurescript "1.11.121"]
                              [org.clojure/clojure       "1.11.1"]]}
+   :c1.12    {:dependencies [[org.clojure/clojure       "1.12.0-alpha9"]]}
    :c1.11    {:dependencies [[org.clojure/clojure       "1.11.1"]]}
    :c1.10    {:dependencies [[org.clojure/clojure       "1.10.1"]]}
    :c1.9     {:dependencies [[org.clojure/clojure       "1.9.0"]]}
-   :test
-   {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]
+
+   :graal-tests
+   {:source-paths ["test"]
+    :main taoensso.graal-tests
+    :aot [taoensso.graal-tests]
+    :uberjar-name "graal-tests.jar"
+    :dependencies
+    [[org.clojure/clojure                  "1.11.1"]
+     [com.github.clj-easy/graal-build-time "1.0.5"]]}
+
+   :dev
+   {:jvm-opts ["-server" "-Dtaoensso.elide-deprecated=true"]
     :global-vars
     {*warn-on-reflection* true
      *assert*             true
@@ -26,29 +39,17 @@
 
     :dependencies
     [[org.clojure/test.check "1.1.1"]
-     [com.taoensso/timbre    "6.3.1"]]}
+     [com.taoensso/timbre    "6.3.1"]]
 
-   :graal-tests
-   {:dependencies [[org.clojure/clojure "1.11.1"]
-                   [com.github.clj-easy/graal-build-time "1.0.5"]]
-    :main taoensso.graal-tests
-    :aot [taoensso.graal-tests]
-    :uberjar-name "graal-tests.jar"}
+    :plugins
+    [[lein-pprint    "1.3.2"]
+     [lein-ancient   "0.7.0"]
+     [lein-cljsbuild "1.1.8"]
+     [com.taoensso.forks/lein-codox "0.10.10"]]
 
-   :dev
-   [:c1.11 :test
-    {:jvm-opts ["-server"]
-     :plugins
-     [[lein-pprint    "1.3.2"]
-      [lein-ancient   "0.7.0"]
-      [lein-cljsbuild "1.1.8"]
-      [com.taoensso.forks/lein-codox "0.10.10"]]
-
-     :codox
-     {:language #{:clojure :clojurescript}
-      :base-language :clojure}}]}
-
-  :test-paths ["test" #_"src"]
+    :codox
+    {:language #{:clojure :clojurescript}
+     :base-language :clojure}}}
 
   :cljsbuild
   {:test-commands {"node" ["node" "target/test.js"]}
@@ -71,6 +72,6 @@
    "build-once" ["do" ["clean"] ["cljsbuild" "once"]]
    "deploy-lib" ["do" ["build-once"] ["deploy" "clojars"] ["install"]]
 
-   "test-clj"   ["with-profile" "+c1.11:+c1.10:+c1.9" "test"]
-   "test-cljs"  ["with-profile" "+test" "cljsbuild"   "test"]
+   "test-clj"   ["with-profile" "+c1.12:+c1.11:+c1.10:+c1.9" "test"]
+   "test-cljs"  ["with-profile" "+c1.12" "cljsbuild"         "test"]
    "test-all"   ["do" ["clean"] ["test-clj"] ["test-cljs"]]})
