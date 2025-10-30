2ada8b5dfab881c6508f54336c03b887588b7cf2
Eugene Tan <contact@eugene.io>
2025-10-30 16:03:15 +0800

    [fix] [#72] Add missing ring-codec and bump Tufte in example project (@jmingtan)

--- examples/clj/project.clj
+++ examples/clj/project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso.examples/tufte "2.7.0"
+(defproject com.taoensso.examples/tufte "3.0.0"
   :description "Tufte example web-app project"
   :url "https://github.com/ptaoussanis/tufte"
   :main    example.server
@@ -7,6 +7,7 @@
   [[org.clojure/clojure "1.12.0"]
    [ring                "1.14.1"]
    [ring/ring-defaults  "0.6.0"]
+   [ring/ring-codec     "1.3.0"]
    [compojure           "1.7.1"]
    [hiccup              "1.0.5"]
-   [com.taoensso/tufte  "2.7.0"]])
+   [com.taoensso/tufte  "3.0.0"]])
